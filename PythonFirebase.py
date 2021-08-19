b1390cb2dc892d3520dbe56d59cd2b210dc2df37
OlegKzoev <67964582+OlegKzoev@users.noreply.github.com>
2021-08-19 13:30:15 +0300

    Add files via upload

---
+++ PythonFirebase.py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+def main():
+    print("welcome!")
+    
+print("Enter your login")
+login = input()
+print("Enter password")
+password = input()
+
+urll = "https://yourfirebaseyrl.firebaseio.com/"
+da = urll + "Users/" + login + "/.json"
+g = requests.get(da)
+if g.text == "null":
+    ip = requests.get('https://api.ipify.org').text
+    r = requests.put(da, data = "{" + '"Password"' + ":" + '"'+password+'"' + "," + '"ip"' + ":" + '"'+ip+'"' "}")
+    main()
+else:
+    ps = urll + "Users/" + login + "/Password" + "/.json"
+    pasw = requests.get(ps).text
+    if '"'+password+'"' != pasw:
+        print("Wrong password!")
+    else:
+        main()
